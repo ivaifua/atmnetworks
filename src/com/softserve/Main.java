0791a0f136404145f22d36b767f3633caa8263a7
Ivanna <ivaifua@users.noreply.github.com>
2014-10-21 10:32:02 +0300

    Update Main.java

--- src/com/softserve/Main.java
+++ src/com/softserve/Main.java
@@ -8,7 +8,7 @@ public class Main {
         ArrayList<AtmNetwork> atmNetworks = new ArrayList<AtmNetwork>(5); // 5 will be enough
 
         atmNetworks.add(new AtmNetwork("ПриватБанк", new PrivatBanksListCreator("https://privatbank.ua/")));
-        atmNetworks.add(new AtmNetwork("АТМоСфера", new AtmosferaBanksListCreator("http://www.atmosphere.net.ua/")));
+        atmNetworks.add(new AtmNetwork("АТМоСфера", new AtmosferaBanksListCreator("http://atmosphere.net.ua/ru/members_atmoshera_rus.htm")));
         atmNetworks.add(new AtmNetwork("Euronet", new EuronetBanksListCreator("http://ubanks.com.ua/atmnet/euronet.php")));
         atmNetworks.add(new AtmNetwork("Радіус", new RadiusBanksListCreator("http://ubanks.com.ua/atmnet/radius.php")));
         atmNetworks.add(new AtmNetwork("УкрКард", new UkrcardBanksListCreator("http://www.ukrcard.com.ua/")));
